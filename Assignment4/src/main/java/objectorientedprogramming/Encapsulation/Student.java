6ccfcc57f37f9aa2ed65c8119aca75e548fc1be2
ThandiswaJikijela <36195934+ThandiswaJikijela@users.noreply.github.com>
2019-06-04 18:44:14 +0200

    Update Student.java

--- Assignment4/src/main/java/objectorientedprogramming/Encapsulation/Student.java
+++ Assignment4/src/main/java/objectorientedprogramming/Encapsulation/Student.java
@@ -4,28 +4,20 @@ public class Student {
     private int studentNo;
     private String name;
 
+    public void setStudentNo(int studentNo) {
+        this.studentNo = studentNo;
+    }
+    
     public int getStudentNo(int studentNo) {
         return studentNo;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+    
     public String getName(String name) {
         return name;
     }
 
 }
-
-   /*class Encapsulation{
-        public static void main(String[] args) {
-
-            Student stud = new Student();
-            stud.setStudentNo(210765534);
-            stud.setName("Gabe");
-            stud.setSurname("Press");
-
-            System.out.println(stud.getStudentNo());
-            System.out.println(stud.getName());
-            System.out.println(stud.getSurname());
-        }
-
-    }
-*/
